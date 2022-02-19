8ba3638fe27f39d15080376752a5fc3fcef1cbad
CHILLBRO4U <chillbrodev444@gmail.com>
2022-02-19 08:43:32 +0530

    Changes in margin

--- src/Components/Solutions/Acts.js
+++ src/Components/Solutions/Acts.js
@@ -2,19 +2,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 export default function Act1() {
+  let Style = {
+    margin : "5vh",
+
+
+  }
 
   return (
     <div >
     <h1 className="text-center">Merchant Of Venice</h1>
   <br></br>
-    <div className="hstack gap-5">
+  <h1 style={Style} className="text-left">Act-1</h1>
+        <br />
+    <div className="hstack gap-4">
          <br />
+       
           <NavLink to="/sc1">
-          <button   className="btn btn-primary btn-lg">Act-1 Scene-1</button>
+          <button   className="btn btn-primary btn-lg">Scene-1</button>
 </NavLink> 
 <div  > </div>
 <NavLink to="/sc2">
-          <button  className="btn btn-primary btn-lg" >Act-1 Scene-2</button>
+          <button  className="btn btn-primary btn-lg" >Scene-2</button>
 </NavLink> 
     </div>
     </div>
