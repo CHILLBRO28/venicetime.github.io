510553a19df2ff253aadb1e00f63c7ef2bae52c6
CHILLBRO4U <chillbrodev444@gmail.com>
2022-02-19 14:11:31 +0530

    Dream...

--- src/Components/Solutions/Act-1/Sc2.js
+++ src/Components/Solutions/Act-1/Sc2.js
@@ -65,6 +65,163 @@ Ans: Country palatine was a powerful lord who came as a suitor to Belmont to try
 Portia says he is sad and solemn. He has an abrupt manner of speech and may become a sad philosopher like Heraclitus when he grows old. She further says that she would rather be married to a skull with a bone in its mouth than to either of those men.
 <br /><br /><br />
 
+<h3 className="text-center">Extract-3</h3>
+
+<strong>i) What has Portia said earlier about the Neapolitan prince and his horse? Which characteristic is common between Monsieur Le Bon and the Count palatine?
+</strong><br />
+Ans: Earlier Portia described the Neapolitan Prince as an inexperienced young man like a young horse.   He talks only about is his horse. He thinks it’s a great credit to his character that he can shoe a horse all by himself. 
+The Count Palatine and Monsieur Le Bon share the common characteristic of frowning and both talk about his horse.
+<br /><br />
+<strong>ii) Give the meaning of: "he is very in no man:"
+</strong><br />
+Ans: A man who has the peculiarities and characteristics of every man but no personality of his own.
+<br /><br />
+<strong>iii) How does the French lord react to the singing of a thrush?
+</strong><br />
+Ans: French lord is so fickle-minded that when he hears the thrush's singing, he immediately starts jumping up and down to that music.
+<br /><br />
+<strong>iv) What would happen:
+(a) if Portia were to marry the Court?
+(b) if he were to despise Portia?
+(c) if he were to love Portia passionately?</strong>
+<br />
+Ans: (a) If Portia were to marry the Court, it would be as if she were married to twenty husbands because he’s like twenty men all rolled into one.
+
+(b) If he were to despise Portia, Portia would not be in loss or anger, she would forgive him.
+
+(c) If he were to love Portia Passionately, Portia could never return his love because she could never possibly love twenty husbands.
+<br /><br />
+<strong>v) Give any three negative qualities of the French lord. What does Portia say about him?
+</strong><br />
+Ans: a)He is a man who has the peculiarities and characteristics of every man but no personality of his own.
+<br />
+b) He is a man who frowned a lot and talks even more than Count-Palatine about his horse.
+<br />
+c) He is a fickle-minded person, who starts jumping on thrush's singing and will fence his own shadow.
+<br /><br /><br />
+
+<h3 className="text-center">Extract-4</h3>
+
+<strong>i) How is the young baron's external appearance described in the passage? What light does it throw on the national pattern of clothes of English men?
+</strong><br />
+Ans: The young baron’s external appearance is described as odd and weird. He lacks individuality in dressing up. it seems that he bought his jacket in Italy, his round hose in France, his hat in Germany, and his behavior everywhere.
+It seems Englishmen of Shakespeare’s days had a fondness for the manners and clothes of foreigners.
+<br /><br />
+
+<strong>ii) What shows that the English lord has poor knowledge of the European languages? Why does Portia find it difficult to interact with him?
+<br /></strong>
+Ans: Portia’s sentiment that the Englishman did not know Latin, French or Italian shows that he had very poor knowledge of European Languages. Portia found it difficult to converse with him as he did not know Latin, French or Italian and Portia’s knowledge of English was very poor.
+<br /><br />
+
+<strong>iii) Give the meaning of:
+(a) He is a proper man's picture:
+(b) How oddly he is suited! :</strong>
+<br />
+Ans: (a) "He is a proper man's picture", it means a man of fine appearance.
+Here Portia referred this line to Young baron's dress sense.<br />
+(b) "How oddly he is suited", it means he dressed himself in a strange way.
+<br /><br />
+<strong>iv) What is referred to as 'a a dumb-show'? Why is the baron said to be a 'dumb-show'?
+</strong><br />
+Ans: Dumb-show refers to mime or acting without a word, that is by the gesture. Here young baron is referred to as a dumb show because he doesn’t speak Latin, French, or Italian, and Portia knows little English so, Englishman had to converse by means of signs as in a dumb show.
+<br /><br />
+<strong>v) Give a brief description of the Scottish Lord. What type of contemporary relationship among England, Scotland, and France is reflected in the description of the Scottish Lord?
+</strong><br />
+Ans: The Scottish Lord is not impressive. Portia speaks about his cowardice in a sarcastic way by calling him kind-hearted as he did not return the Englishman’s blow immediately.
+The description of the Scottish Lord is a reference to the frequent alliances between the Scots and the French and England, once at war Englishman slap Scottish Lord on the ear without hitting him back. Rather than defend himself, he just threatened to pay the Englishman back later. Then the Frenchman promised to help the Scot pay the Englishman back, and added a slap of his own.
+
+<br /><br /><br />
+<h3 className="text-center">Extract-5</h3>
+
+<strong>i) How does the young German behave when he is sober and when he is drunk? If the worse happens to Portia which would compel her to marry him, what would she do then?
+</strong><br />
+Ans: Young German is very disgusting in the morning when he is sober and most disgusting in the afternoon when he is drunk. If the worst happens to Portia which would compel her to marry him, then she is sure that she could find a way to go on without him. 
+<br /><br />
+<strong>ii) Give the meaning of:</strong>
+<br />
+(a) When he is worst, he is little better than a beast.<br />
+(b) set a deep glass of Rhenish wine on the contrary casket.
+<br />
+
+
+Ans: (a) When he is drunk, he is almost beast, crude and uncivilized like an animal.<br />
+         (b) It means Place a large glass of Rhenish wine on the wrong casket. Portia says that a glass full of Rhenish wine should be kept on the wrong casket so that it gives him temptation and he chooses the wrong casket.
+
+         <br /><br />
+<strong>iii) What plan does Portia make to prevent the young German from choosing the right casket?</strong>
+<br />
+Ans: Portia instructed Nerissa to place a large glass of Rhenish wine on the wrong casket to tempt him and deviate his attention from choosing the right casket. Portia was sure that the German suitor will not be able to resist the temptation if the picture of the devil himself was within and he will choose the wrong casket.
+<br /><br />
+<strong>iv) Why is the young German referred to as 'a sponge'?</strong>
+<br />
+Ans: Sponge is one which soaks up or absorbs liquid. Young German, all through the day is under the influence of liquor. Since he is a drunkard, who absorbs or soaks himself up in the liquor, he is referred to as 'a a sponge'. Portia calls him a sponge.
+<br /><br />
+
+<strong>v) After Portia's speech, what does Nerissa say to console her about the suitors? What conclusion can you draw about Nerissa's speech in the context?
+</strong><br />
+Ans: Nerissa consoles Portia by giving the most relieving news that she need not to worry about suitors. They’ve all told her what they want. They are going back home and give up on Portia. They wanted to win her by some other way and not as her father commanded.
+<br /><br />
+
+
+
+<br /><br /><br />
+<h3 className="text-center">Extract-6</h3>
+
+<strong>i) What was Portia's father's will as far as Portia's marriage was concerned?</strong>
+<br />
+Ans: The "lottery of caskets" was devised according to the will of Portia's father. According to the will of her father, each suitor has to choose from amongst the three caskets of gold, silver, and lead. The one who makes the right choice would marry Portia.
+
+<br /><br />
+<strong>ii) Who is Sibylla and Who is Diana? Who are they referred to in the context?</strong>
+<br />
+Ans: Sibylla is a Greek word which means "prophetess". According to the stories recounted in Ovid's Metamorphoses, Sibyl of Cumae was granted a wish by goddess Apollo that she would live for as many years as the grains of sand she held in her hand. She was the ageless old woman.
+Diana means the "Bright Goddess". Diana was the goddess of the moon and of hunting. She is also known as the Virgin Goddess.
+A reference is made to Sibylla and Diana to show that Portia would like to remain unmarried, if nobody is able to choose the right casket, in order to keep the will of her father. It shows her willingness and loyalty to the will of her father.
+<br /><br />
+
+<strong>iii) What does Nerissa say to introduce Bassanio? What was the feeling of Portia for Bassanio in this scene?</strong>
+<br />
+Ans: In order to introduce Bassanio, Nerissa asks Portia, "Do you remember, lady in your father' time, a Venetian, a scholar and a soldier that come hither in company of the Marquis of Montferrat?"
+In this scene, Portia’s feelings for Bassanio are quite positive. She recalls his name and tells Nerissa that he fully deserves her praise. Her simple reply shows that she is already in love with Bassanio.
+
+<br /><br />
+<strong>iv) In what sense can we say that the opening of the first two scene of Act 1 gives the plots of the play?</strong>
+<br />
+Ans: The first scene starts in the street of Venice and in the second scene in Portia's house at Belmont is introduced, these are the two important places where the whole story is taking place.
+The opening of the first two scenes of Act I gives the plot of the play. The two main plots of the play are the bond story and the casket story. The bond story is initiated in Scene 1 while the casket story is initiated in Scene 2 of Act I. These two scenes set the plot of the play perfectly.
+<br /><br />
+<strong>v) Name the six suitors given in this scene, Give two characteristics of any two suitors described by Portia.</strong>
+<br />
+
+Ans: (i) The Neapolitan Prince(from Naples, Italy)<br />
+⇒ He is inexperienced like a young horse.<br />
+⇒ He talks only about is his horse. He thinks it’s a great credit to his character that he can shoe a horse all by himself.
+<br /><br />
+
+(ii) Count palatine<br />
+⇒ He does nothing but frown. he is full of unmannerly sadness in his youth age.<br />
+⇒ He doesn’t even smile when he hears funny stories.
+
+<br /><br />
+(iii) Monsieur Le Bon<br />
+⇒ He shares the common characteristic of frowning and talking about his horse like Count Palatine<br />
+⇒ He is a man who has the peculiarities and characteristics of every man but no personality of his own.
+<br /><br />
+
+(iv) Baron Falconbridge, the young baron of England<br />
+⇒  Strangely and unmannerly dressed.<br />
+⇒ He speaks such languages that are not known to Portia.
+<br /><br />
+
+(v) Scottish Lord:<br />
+⇒ He is not pleasant and is a coward.<br />
+⇒He let the Englishman slap him on the ear without hitting him back.
+<br /><br />
+
+(vi) The young German, The Duke of Saxony's nephew.<br />
+⇒ He is a drunkard.<br />
+⇒ When he is drunk, he is almost beast, crude and uncivilized like an animal
+<br />
 </p> 
 
 </div>
