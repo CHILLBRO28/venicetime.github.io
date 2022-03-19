692d03dbfd828f775632d69ba07577332251055e
CHILLBRO4U <chillbrodev444@gmail.com>
2022-03-19 13:21:39 +0530

    title

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AboutUs() {
   return (
     <div>
-      This Website is made for providing Merchant Of Venice work book answers to students.
+      This Website is made for providing Merchant Of Venice work book Solutions to students.
     </div>
   )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,7 +14,7 @@ function Home() {
     <div className="App" style={Style}>
 
           <NavLink to="/Acts">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHhdW6z2_2CkaEkndEnyJ7FeaXjNk4vhgnQg"  className="shadow p-3 mb-5 bg-body rounded rounded mx-auto d-block"></img>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHhdW6z2_2CkaEkndEnyJ7FeaXjNk4vhgnQg"  className="shadow p-3 mb-5 bg-body rounded rounded mx-auto d-block" title="Merchant Of Venice"></img>
           <h4 className="text-center">Merchant Of Venice</h4>
 
 </NavLink>
